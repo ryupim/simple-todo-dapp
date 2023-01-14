68240f73bdd1b11070b47d94b173ee265d2e4e17
ryupim <shingai92@gmail.com>
2023-01-14 22:37:18 +0900

    Refer to task content

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,25 +5,38 @@ import React, { FC, useEffect, useState } from 'react';
 
 import artifact from './abi/TodoList.json';
 
-// type Task = {
-//     id: string;
-//     content: string;
-//     isCompleted: boolean;
-// };
+type Task = {
+    id: string;
+    content: string;
+    isCompleted: boolean;
+};
 
 const useContent = (contract: ethers.Contract) => {
-    const { taskCount } = contract.functions;
+    const { taskCount, tasks } = contract.functions;
     const [taskContValue, setTaskCountValue] = useState<string>("");
+    const [tasksValue, setTasksValue] = useState<Task[]>([]);
+
     useEffect(() => {
-        const getTaskCount = async () => {
+        const getTasks = async () => {
             const _taskCount = await taskCount();
             setTaskCountValue(_taskCount);
+
+            const _tasks = [];
+            for (let i = 1; i <= _taskCount; i++) {
+                const _task = await tasks(i);
+                _tasks.push({
+                    ..._task,
+                    id: i,
+                });
+            }
+            setTasksValue(_tasks);
         };
-        getTaskCount();
+        getTasks();
     }, []);
 
     return {
         taskCount: taskContValue,
+        tasks: tasksValue,
     };
 };
 
@@ -32,8 +45,32 @@ type ContentProps = {
 };
 
 const Content: FC<ContentProps> = ({ contract }) => {
-    const { taskCount } = useContent(contract);
-    return <p>{`taskCount ... ${taskCount}`}</p>;
+    const { taskCount, tasks } = useContent(contract);
+    return (
+        <div>
+            <p>{`taskCount ... ${taskCount}`}</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Content</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tasks.map((t, index) => (
+                        <tr key={`task.${index}`}>
+                            <td>{t.id}</td>
+                            <td>{t.content}</td>
+                            <td>
+                                {t.isCompleted ? "Completed" : "Not Completed"}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
